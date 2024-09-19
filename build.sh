655823ff98ee16c8e746f6d787888c1fe21727ac
Maker Zone <dev@makerzone.com.au>
2024-09-19 11:55:47 +1000

    Removing build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the DisplayValueCommon library using Arduino CLI
-# Skip examples as it's an abstract library
-
-arduino-cli compile --libraries . --fqbn arduino:avr:uno
-
-echo "DisplayValueCommon library compiled successfully."
