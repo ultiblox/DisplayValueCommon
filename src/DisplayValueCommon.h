6b6736e5a246b8b37a97e823a6dbf0bd44db232e
Maker Zone <dev@makerzone.com.au>
2024-09-23 10:06:56 +1000

    Changing updateValue to setValue and using char arrays instead of strings

--- src/DisplayValueCommon.h
+++ src/DisplayValueCommon.h
@@ -8,8 +8,10 @@ public:
     virtual ~DisplayValueCommon() {}
     virtual void init() = 0;
     virtual void clear() = 0;
-    virtual void setLabel(const String& label) = 0;
-    virtual void updateValue(const String& value) = 0;
+    virtual void setLabel(const char* value) = 0;
+    virtual void setValue(const char* value) = 0;
+    virtual void setValue(int value) = 0;
+    virtual void setSuffix(const char* value) = 0;
 };
 
 #endif
